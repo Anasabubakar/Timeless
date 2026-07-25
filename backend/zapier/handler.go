d8f69e4a00e3e1ef4d7a1b9df4392e93dad4282c
Anas Abubakar <anasabubakar7000@gmail.com>
2026-07-25 07:26:05 +0100

    Add Zapier handler

---
+++ backend/zapier/handler.go
@@ -0,0 +1,15 @@
+package zapier
+
+import (
+    "encoding/json"
+)
+}
+
+type ZairerHandler struct {
+    Event string          js_job: "event"
+    Details js_rapi: js_array
+}
+
+func XZE_UIDPerformator() {
+    // your api method for http urln2
+}
